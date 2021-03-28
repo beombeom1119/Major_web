5eba545d7a531f16280751a9b12e403c1a09c0a8
qjarl1119 <qjarl1119@naver.com>
2021-03-28 14:28:42 +0900

    화면에 결과값 가능

---
+++ client/src/components/WelcomePage-motion.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react'
+import * as tmImage from '@teachablemachine/image'
+
+export default function WelcomePage() {
+  const [openVal, setOpenVal] = useState(false);
+  const [state, setstate] = useState({
+    result1 : "1번",
+    result2 : "2번",
+  })
+
+  const URL = "https://teachablemachine.withgoogle.com/models/p01fU9lVr/"
+  let model;
+  let webcam;
+  let labelContainer;
+  let maxPredictions;
+
+  useEffect(() => {
+    init()
+  }, [])
+
+  async function init() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+
+    const flip = true; 
+    webcam = new tmImage.Webcam(20, 20, flip); 
+    await webcam.setup();
+    await webcam.play();
+    window.requestAnimationFrame(loop);
+
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
+    labelContainer = document.getElementById("label-container");
+    for (let i = 0; i < maxPredictions; i++) { 
+      labelContainer.appendChild(document.createElement("div"));
+    }
+  }
+
+  async function loop() {
+    webcam.update();
+    await predict();
+    window.requestAnimationFrame(loop);
+  }
+
+  async function predict() {
+    
+    console.log(state.result1+"1번")
+    console.log(state.result2+"2번")
+    const prediction = await model.predict(webcam.canvas);
+    console.log("핸드폰일 확률"+prediction[0].probability.toFixed(2))
+    console.log("지갑일 확률"+prediction[1].probability.toFixed(2))
+    setstate({
+      result1 : prediction[0].probability.toFixed(2),
+      result2 : prediction[1].probability.toFixed(2),
+    })
+    if (prediction[0].probability.toFixed(2) >= 0.7) {
+      setOpenVal(true);
+      // labelContainer.childNodes[0].innerHTML = prediction[0].probability.toFixed(2)
+
+      // labelContainer.childNodes[0].innerHTML = '열어줘라!'};
+    } else {
+      setOpenVal(false);
+    }
+  }
+
+  return (
+    <div>
+      <div id="webcam-container"></div>
+      <div id="label-container"><h3>정상 확률 : {state.result1}  탈모 확률 :{state.result2}</h3></div>
+      <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.3.1/dist/tf.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/@teachablemachine/image@0.8/dist/teachablemachine-image.min.js"></script>
+    </div>
+  )
+}
